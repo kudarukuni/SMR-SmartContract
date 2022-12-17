b07c0f9acaf7e4fbb4c80e5137148bf3e6893767
kudarukuni <krukuni@gmail.com>
2022-12-17 14:21:22 +0200

    add reply feauteres on ssis

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,17 @@
-use solana_program::{program_error::ProgramError};
+use solana_program::program_error::ProgramError;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum ReviewError{
-    // Error 0
+pub enum ReviewError {
     #[error("Account not initialized yet")]
     UninitializedAccount,
-    // Error 1
+
     #[error("PDA derived does not equal PDA passed in")]
     InvalidPDA,
-    // Error 2
+
     #[error("Input data exceeds max length")]
     InvalidDataLength,
-    // Error 3
+
     #[error("Rating greater than 5 or less than 1")]
     InvalidRating,
 }
